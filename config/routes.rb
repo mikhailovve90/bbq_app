2613c439c8d011ef0c4bbe9c54e046722d170527
mikhailovve90 <mikhailov.ve90@gmail.com>
2017-10-04 18:29:29 +0300

    init user

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   root "events#index"
 
   resources :events
+  resources :users, only: [:show, :edit, :update]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
